2977be3f2aef712ce748b8967b6406e981d06508
Sanya <sanyagondi1308@gmail.com>
2021-12-28 12:38:11 +0530

    DrinkingNotifier Project

---
+++ DrinkingWaterNotifier.py
@@ -0,0 +1,4 @@
+from plyer import notification
+title = "Drink Water!!"
+message = "Drinking 8 glass of water is beneficial for your health"
+notification.notify(title = title, message = message, app_icon = None, timeout = 10, app_name = "HP")
